431ccab84a5e858b74ec869ae52ea01ee2e456c0
Usuario <usuario@email.com>
2025-02-05 21:14:09 -0300

    Agregando componente AddTask

--- src/components/Logos.jsx
+++ src/components/Logos.jsx
@@ -1,7 +1,7 @@
 import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
 
-export const Logos = ()=>{
+export const Logos = ({ title })=>{
     return <div>
     <a href="https://vite.dev" target="_blank">
       <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -9,5 +9,7 @@ export const Logos = ()=>{
     <a href="https://react.dev" target="_blank">
       <img src={reactLogo} className="logo react" alt="React logo" />
     </a>
+
+    <h1>{title}</h1>
   </div>
 }
